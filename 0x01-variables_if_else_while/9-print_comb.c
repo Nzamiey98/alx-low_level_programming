629fdefa9f533c7d17b1999f62ef0038f4bd51c2
Nzamiey98 <nzamiey.com@gmail.com>
2023-02-19 04:28:10 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry
+ * main - Entry point
  * Return: 0 (Success)
  */
 int main(void)
 {
-	int c;
-	int d = 0;
+	int max_num = 10;
+	int counter = 0;
 
-	while (d < 10)
+	while (counter < max_num)
 	{
-		c = 0;
-		while (c < 10)
+		/**
+		 * if (counter != 0 && (counter % 11 == 0))
+		 * continue;
+		 */
+		putchar('0' + counter);
+
+		if (counter != max_num - 1)
 		{
-			if (d != c && d < c)
-			{
-				putchar('0' + d);
-				putchar('0' + c);
-
-				if (c + d != 17)
-				{
-					putchar(',');
-					putchar(',');
-				}
-			}
-
-			c++;
+			putchar(',');
+			putchar(' ');
 		}
-		d++;
+		else
+		{
+			putchar('\n');
+		}
+
+		counter++;
 	}
-	puthchar('\n');
 	return (0);
 }
