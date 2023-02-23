3598689189059d1899a149cef74970e9050ad7d3
Nzamiey98 <nzamiey.com@gmail.com>
2023-02-23 11:26:00 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,22 +8,22 @@ void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
-	_putchar('\n');
-	{
+		_putchar('\n');
+	}
 	else
-	}
-	int i, j;
+	{
+		int i, j;
 
-	for (i = 0; i < n; i++)
-	{
-	for (j = 0; j < n; j++)
-	{
-	if (j == i)
-	_putchar('\\');
+		for (i = 0; i < n; i++)
+		{
+		for (j = 0; j < n; j++)
+		{
+	if (j == i)	
+		_putchar('\\');
 	else if (j < i)
-	putchar(' ');
-	}
-	_putchar('\n');
-	}
+		_putchar(' ');
+		}
+		_putchar('\n');
+		}
 	}
 }
